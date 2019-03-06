55dc6fd918b9438df401d1a090f2db245d2d1caf
TashanStudio <48272776+TashanStudio@users.noreply.github.com>
2019-03-06 20:25:30 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,9 +11,9 @@ bot.on('message', message =>{
     var msg = message.content;
     var prefix = "+" //The text before commands, you can put anything that you prefer
 
-    if(message.author.id != "552731436591874049" && message.channel.id === "381397780385628181"){
+    if(message.author.id != "552731436591874049" && message.channel.id === "536913505283211276"){
         if(msg.startsWith('+', 0)){
-            if(msg === prefix + "help" && message.channel.id === "381397780385628181"){
+            if(msg === prefix + "help" && message.channel.id === "536913505283211276"){
                 message.channel.send('Welcome To Only Trivia Answers At That Time Bot Is On Rest And In maintenance So Please DM To Admin Thanks') // Sends a message to the channel, with the content
             }
             else{
